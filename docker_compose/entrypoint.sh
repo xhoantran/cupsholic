7d905d5663b8ae1f594bd39ee74d395442ae76ae
Hoan Tran <xhoantran@Hoans-MacBook-Pro.local>
2021-10-28 21:31:20 -0400

    psycopg2-binary

--- docker_compose/entrypoint.sh
+++ docker_compose/entrypoint.sh
@@ -20,23 +20,23 @@ fi
 # Need to update the DATABASE_URL if using DOCKER
 export DATABASE_URL=postgres://$POSTGRES_USER:$POSTGRES_PASSWORD@postgres:5432/$POSTGRES_DB
 
-# function postgres_ready(){
-# python << END
-# import sys
-# import psycopg2
-# try:
-#     conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
-# except psycopg2.OperationalError:
-#     sys.exit(-1)
-# sys.exit(0)
-# END
-# }
+function postgres_ready(){
+python << END
+import sys
+import psycopg2
+try:
+    conn = psycopg2.connect(dbname="$POSTGRES_DB", user="$POSTGRES_USER", password="$POSTGRES_PASSWORD", host="postgres")
+except psycopg2.OperationalError:
+    sys.exit(-1)
+sys.exit(0)
+END
+}
 
-# until postgres_ready; do
-#     >&2 echo "Postgres is unavailable - sleeping"
-#     sleep 10
-# done
-# >&2 echo "Postgres is up - continuing..."
+until postgres_ready; do
+    >&2 echo "Postgres is unavailable - sleeping"
+    sleep 10
+done
+>&2 echo "Postgres is up - continuing..."
 
 sleep 10
 exec $cmd
